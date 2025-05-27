72d09a61afe19a4a59037b93e9512c771b01bd95
Nicolò Festa <nicolof@monade.io>
2025-05-27 14:45:19 +0200

    add self link to serializer

--- api/app/models/application_record.rb
+++ api/app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  
+  def resource_name
+    self.class.name.pluralize.downcase
+  end
+
+  def resource_link
+    "/#{resource_name}/#{self.id}"
+  end
 end
--- api/app/serializers/base_serializer.rb
+++ api/app/serializers/base_serializer.rb
@@ -2,4 +2,8 @@ class BaseSerializer
   include JSONAPI::Serializer
 
   set_key_transform :camel_lower
+
+  link :self do |object|
+    object.resource_link
+  end
 end
