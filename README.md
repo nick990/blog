b92a9d0e046cbe249695bba4e627ec597bb0329b
Nicolò Festa <nicolof@monade.io>
2025-05-27 17:39:59 +0200

    add link to relationships

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # blog
+
 monorepo rails + vue
+
+# TODO
+
+cancancan
+meta dati nella relationship (es. count dei comments su article)
--- api/app/serializers/article_serializer.rb
+++ api/app/serializers/article_serializer.rb
@@ -1,5 +1,16 @@
 class ArticleSerializer < BaseSerializer
   attributes :id, :title, :body
-  belongs_to :author
-  has_many :comments
+
+  belongs_to :author, links: {
+    related: -> (object) {
+      "#{object.author.resource_link}"
+    }
+  }
+  
+  has_many :comments, lazy_load_data: true, links: {
+    related: -> (object) {
+      "#{object.resource_link}/comments"
+    }
+  }
+
 end
