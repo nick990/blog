73b0665b4675be705994cc710c422e10ddef7770
Nicolò Festa <nicolof@monade.io>
2025-05-27 17:55:51 +0200

    handle null author in article serializer

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ monorepo rails + vue
 
 # TODO
 
-cancancan
-meta dati nella relationship (es. count dei comments su article)
+-cancancan
+-meta dati nella relationship (es. count dei comments su article)
+-resource name statico? come faccio a fare override e fare in modo che venga preso il più in basso nella gerarchia?
--- api/app/serializers/article_serializer.rb
+++ api/app/serializers/article_serializer.rb
@@ -2,15 +2,16 @@ class ArticleSerializer < BaseSerializer
   attributes :id, :title, :body
 
   belongs_to :author, links: {
-    related: -> (object) {
-      "#{object.author.resource_link}"
+    related: ->(object) {
+      object.author&.resource_link
     }
   }
   
   has_many :comments, lazy_load_data: true, links: {
-    related: -> (object) {
-      "#{object.resource_link}/comments"
+    related: ->(object) {
+      object.resource_link + '/comments'
     }
+  }, meta: ->(object, params) {
+    { count: object.comments.count }
   }
-
 end
